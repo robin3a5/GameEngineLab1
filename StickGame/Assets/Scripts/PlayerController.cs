0da1c55150c25b9762fa06769cd47be4d0a4c2ce
Alex Robinson <ajrobinson232@gmail.com>
2021-06-06 19:10:19 -0400

    A lot of changes
    Changed the inventory asset to one image for consistency, added a scene change, created a prefab folder, updates to colliders for main character and table, added new scene, added new assets

--- StickGame/Assets/Scripts/PlayerController.cs
+++ StickGame/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 target;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
-            target = new Vector2(mousePos.x, transform.position.y );
+            target = new Vector2(mousePos.x, mousePos.y );
         }
         transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
     }
